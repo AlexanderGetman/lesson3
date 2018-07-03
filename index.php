92c258642ae67f31951fb255eeebdb796382804d
AlexanderGetman <40514260+AlexanderGetman@users.noreply.github.com>
2018-07-03 22:47:47 +1000

    Update index.php

--- index.php
+++ index.php
@@ -7,57 +7,22 @@ $animals = array(
     "Antarctica" => array("Arctocephalus gazella", "Orcinus orca")
 );
 
-$latinClassification = [];
 foreach ($animals as $continents => $all_species) {
     foreach ($all_species as $species) {
         $species_separated = [];
         $species_explode = explode(' ', $species);
-        $species_separated[] = $species_explode;
-
-        foreach ($species_separated as $species_wordcount) {
-            if (count($species_wordcount) === 2) {
-                $species_comma = implode(",", $species_wordcount);
-                $species_str = str_replace(',', ' ', $species_comma);
-                $latinClassification[] = $species_str;
-            }
-        }
+        $latinClassification[] = $species_explode;
     }
 }
 
-
-foreach($latinClassification as $name_parts){
-    $parts = explode(' ', $name_parts);
-    $first[] = $parts[0];
-    $second[] = $parts[1];
-}
-
-$random_first_word = [];
-while (count($random_first_word) < count($latinClassification)){
-    $check = $first[rand(0, count($latinClassification)-1)];
-    if (!in_array($check, $random_first_word)) {
-        array_push($random_first_word, $check);
+foreach ($latinClassification as $speciesCount){
+    if (count($speciesCount)===2){
+        $species_comma = implode(",", $speciesCount);
+        $species_str = str_replace(',', ' ', $species_comma);
+        $latinClassificationTwo[] = $species_str;
     }
 }
 
-$random_second_word = [];
-while (count($random_second_word) < count($latinClassification)){
-    $check = $second[rand(0, count($latinClassification)-1)];
-    if (!in_array($check, $random_second_word)) {
-        array_push($random_second_word, $check);
-    }
-}
-
-
-$latinClassificationFantasy = [];
-for($i = 0; $i < count($latinClassification); $i++){
-    $latinClassificationFantasy[]= $random_first_word[$i] . ' ' . $random_second_word[$i];
-}
-
-for($i = 0; $i < count($latinClassificationFantasy); $i++){
-    echo "$latinClassificationFantasy[$i]";
-    if($i < (count($latinClassificationFantasy) -1)){
-        echo ", ";
-    }
-}
+print_r ($latinClassificationTwo);
 
 ?>
